e406b47110d2af6288c5c72ec80cfcd062fdfd83
6eeesh <144094732+6eeesh@users.noreply.github.com>
2023-12-27 12:33:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication53/ConsoleApplication53.cpp
@@ -0,0 +1,28 @@
+﻿int main()
+{
+	stack<int> st;
+
+	for (int i = 1; i <= 5; i++)
+	{
+		st.push(i);
+	}
+
+	cout << "Top element: " << st.top() << "\n";
+
+	for (int i = 0; i < 2; i++)
+	{
+		if (!st.empty())
+		{
+			cout << "Popped elements: " << st.top() << "\n";
+			st.pop();
+		}
+	}
+
+	cout << "All elements: ";
+	while (!st.empty())
+	{
+		cout << st.top() << " ";
+		st.pop();
+	}
+
+}
